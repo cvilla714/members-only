84c7e8332f6b03d6dec8e963999e971eccab6509
thneves <tsneves11@gmail.com>
2020-12-07 17:40:39 -0300

    change routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
-  resources :posts, only:[:new, :edit, :index]
+  resources :posts, only:[:new, :edit, :index, :create]
   
   root "posts#index"
 end
